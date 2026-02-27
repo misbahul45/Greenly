099a7566a7a7266b81bb26827762dc43f32dc909
taqin <misbahulmuttaqin395@gmail.com>
2026-02-27 09:51:16 +0700

    setip auth for user

---
+++ apps/app/lib/core/router/auth_routes.dart
@@ -0,0 +1,7 @@
+class AuthRoutes {
+  static const String login = '/login';
+  static const String register = '/register';
+  static const String forgotPassword = '/forgot-password';
+  static const String verifyEmail = '/verify-email';
+  static const String changePassword = '/change-password';
+}
