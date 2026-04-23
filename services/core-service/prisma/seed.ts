5cc78915c58582a1bc38c29e4b7f1b003aa3ad44
taqin <misbahulmuttaqin395@gmail.com>
2026-04-23 09:45:00 +0700

    done

---
+++ services/core-service/prisma/seed.ts
@@ -0,0 +1,36 @@
+import { PrismaMariaDb } from '@prisma/adapter-mariadb'
+
+export function createPrismaAdapter() {
+  return new PrismaMariaDb({
+    host: process.env.DATABASE_HOST,
+    port: parseInt(process.env.DATABASE_PORT || '3306'),
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    connectionLimit: 5,
+  })
+}
+
+import { PrismaClient } from '../generated/prisma/client'
+import { seedRbac } from './seeds/role.seed'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
+const prisma = new PrismaClient({
+  adapter: createPrismaAdapter(),
+})
+
+async function main() {
+  await prisma.$connect()
+  await seedRbac(prisma)
+}
+
+main()
+  .catch((e) => {
+    console.error('❌ Seed Error:', e)
+    process.exit(1)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
